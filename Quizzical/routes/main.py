4481ba7e6710103f0b9f22ea7b6b6bb0ab9e7378
Adedotun Oni <adedotunoni@MacBook-Pro-4.local>
2019-12-10 04:33:18 -0500

    new questions persisting to DB confirmed

--- Quizzical/routes/main.py
+++ Quizzical/routes/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user, login_required
 
 from Quizzical.extensions import db
 from Quizzical.models import Question, User
@@ -12,14 +13,37 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('home.html')
 
-@main.route('/ask')
+
+
+@main.route('/ask', methods=['GET', 'POST'])
+@login_required
 def ask():
+    if request.method == 'POST':
+        question = request.form['question']
+        expert = request.form['expert']
+
+        question = Question(
+            question=question, 
+            expert_id=expert, 
+            asked_by_id=current_user.id
+        )
+        
+        db.session.add(question)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))
+
     experts = User.query.filter_by(expert=True).all()
 
     context = {
         'experts' : experts
     }
-    return render_template('/ask.html', **context)
+
+    return render_template('ask.html', **context)
+
+
+
+
 
 @main.route('/answer')
 def answer():
